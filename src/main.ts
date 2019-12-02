aaea90ca9db47ff15c9bbdef2442eb080ad170bc
Guo Longxiang, (Longxiang.Guo@partner.bmw.com) <Longxiang.Guo@partner.bmw.com>
2019-12-02 18:45:56 +0800

    integration packages success

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,32 @@ import Router from 'koa-router';
 import { config } from './config';
 import { Service } from './services';
 import { DiscoverService } from 'discover1-node-pacakge';
+
 const app = new Koa();
 const router = new Router();
+
+router.get('/api/v1/discover', async (ctx) => {
+    const service = new DiscoverService();
+    const appKey1 = ctx.get('X-AppKey');
+    const appUsid1 = ctx.get('x-btcapi-usid');
+    const headers1 = {
+        'X-AppKey': appKey1,
+        'x-btcapi-usid': appUsid1
+    }
+    const appVersion = ctx.get('AppVersion');
+    const appKey = ctx.get('AppKey'); 
+    const path = ctx.path;
+    const params = ctx.query;
+    const headers = {
+        'AppVersion': appVersion,
+        'AppKey': appKey
+    }
+    const [news, services] = await Promise.all([
+        service.getNews(config.news_url, headers1),
+        service.getDiscover(config.service_url, headers, params)
+    ])
+    ctx.body = {"news": news, "services": services};
+})
 
 router.get('/api/news', async (ctx) => {
     const service = new Service();
@@ -14,6 +38,7 @@ router.get('/api/news', async (ctx) => {
 router.get('/api/discover', async (ctx) => {
     const service = new Service();
     //异步同时返回
+  
     const [news, services] = await Promise.all([service.getNews(), service.getDiscover()]);
      ctx.body = {"news": news, "services": services};
     })
